4c76ad207da17c9b01d717cd0ff1b213cd478060
Kumataro-K <Kumataro-K@users.noreply.github.com>
2026-04-02 19:17:13 +0900

    RUNができない

--- LifeSurvivor/build.gradle.kts
+++ LifeSurvivor/build.gradle.kts
@@ -1,15 +1,10 @@
 // Life Survivor: 生命38億年の戦い
 // ルートビルドファイル
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.9.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-    }
+plugins {
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
 allprojects {
